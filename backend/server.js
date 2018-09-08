41bc1334523ef5d23363bec2936128547ef37aee
Chris Anstey <git@cpanstey.co.uk>
2018-09-08 14:22:19 +0100

    Extending the mock data a bit... Can now request data for a block
    And it adds a date field (just the current date).

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const moment = require('moment')
 
-const app = express();
+const app = express()
 
 // allow our frontend to access the API
 var corsOptions = {
@@ -11,7 +12,15 @@ var corsOptions = {
 app.use(cors(corsOptions))
 
 // an initial demo endpoint to return some demo data
-var demoBlocks = [{id: 123, name: "Latest Block"},{id: 122, name: "Previous Block"}]
+var demoBlocks = [{id: 123, name: "Latest Block"},{id: 122, name: "Previous Block"},{id: 121, name: "Older Block"}]
 app.get('/', (req, res) => res.send(JSON.stringify(demoBlocks)));
+app.get(
+  '/blocks/:block',
+  (req, res) => {
+    block = demoBlocks.find(o => o.id == req.params.block)
+    block.date = moment().format('MMMM Do YYYY, h:mm:ss a')
+    res.send(JSON.stringify(block))
+  }
+);
 
 app.listen(4000, () => console.log(`Express server running on port 4000`));
